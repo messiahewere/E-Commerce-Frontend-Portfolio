6cb8860e97635aeadada2b0d39cc229e72ddefc2
Messiah Ewere <messiahewere@gmail.com>
2025-11-25 09:15:42 -0500

    populate the header and footer with the necessary contents

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatMenuModule, MatBadgeModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
