6a75adad0796dfb52829da150cbeabcc5eab6b96
Messiah Ewere <messiahewere@gmail.com>
2025-11-26 07:16:17 -0500

    generate most of the pages - home, products,  carts, orders, login and dashboard and also create the route path for each page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadChildren: () => import('./pages/home/home').then(m => m.Home)
+    },
+    {
+        path: 'products',
+        loadChildren: () => import('./pages/products/products').then(m => m.Products)
+    },
+    {
+        path: 'carts',
+        loadChildren: () => import('./pages/carts/carts').then(m => m.Carts)
+    },
+    {
+        path: 'orders',
+        loadChildren: () => import('./pages/orders/orders').then(m => m.Orders)
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login').then(m => m.Login)
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+    }
+];
