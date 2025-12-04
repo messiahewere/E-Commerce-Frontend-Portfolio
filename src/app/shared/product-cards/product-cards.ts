6d0695031e298cc04c015bfdb7ebd90d6957c176
Messiah Ewere <messiahewere@gmail.com>
2025-12-04 09:10:38 -0500

    modify the product-cards rating element to contain a star icon, modified the search/category filter

--- src/app/shared/product-cards/product-cards.ts
+++ src/app/shared/product-cards/product-cards.ts
@@ -1,31 +1,47 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import ProductsModel from '../../models/products';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Search } from '../../services/search';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'product-cards',
-  imports: [CommonModule, CurrencyPipe],
+  imports: [CommonModule, CurrencyPipe, MatIcon],
   templateUrl: './product-cards.html',
   styleUrl: './product-cards.scss',
 })
-export class ProductCards implements OnInit {
+export class ProductCards implements OnInit, OnChanges {
   @Input({ required: true })
   products!: ProductsModel[];
-  showProducts: ProductsModel[] = [];
+  showProducts!: ProductsModel[];
+  currentSearchValue: string = '';
 
   search = inject(Search);
 
   ngOnInit() {
-    this.showProducts = this.products;
-    
     // subscribe to the search input value from the Search service
     this.search.searchEmit.subscribe((searchValue: string) => {
-      if (searchValue) {
-        this.showProducts = this.products.filter((product: ProductsModel) => product.title.toLowerCase().includes(searchValue.toLowerCase()));
-      } else {
-        this.showProducts = this.products;
-      }
+      this.currentSearchValue = searchValue;
+      this.applyFilters();
     });
- }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['products'] && this.products) {
+      this.applyFilters();
+    }
+  }
+
+  private applyFilters(): void {
+    if (!this.products) return;
+    if (this.currentSearchValue) {
+      this.showProducts = this.products.filter((product: ProductsModel) => 
+        product.title.toLowerCase().includes(this.currentSearchValue.trim().toLowerCase())
+      );
+     
+    } else {
+      this.showProducts = this.products;
+    }
+  }
+
 }
