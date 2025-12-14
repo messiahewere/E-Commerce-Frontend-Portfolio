beebf62672e090aefa99d299b3f272e9f1e0bd63
Messiah Ewere <messiahewere@gmail.com>
2025-12-14 07:06:49 -0500

    create the login page for account registration and login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  isLogin: boolean = false;
+
+  onFormSubmit(regLog: NgForm): void {
+    console.log(regLog);
+  }
+
+  onRegLogChange() {
+    this.isLogin = !this.isLogin;
+  }
 
 }
